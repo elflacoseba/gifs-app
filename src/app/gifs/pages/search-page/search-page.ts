439255a55f0957f687de553212eb48c96d928474
Sebastián Serrisuela <sebaserri@gmail.com>
2025-11-15 21:27:28 -0300

    Creando la página de búsqueda y mostrando los gifs buscados.

--- src/app/gifs/pages/search-page/search-page.ts
+++ src/app/gifs/pages/search-page/search-page.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { GifList } from '../../components/gif-list/gif-list';
+import { GifService } from '../../services/Gifs.Service';
+import type { Gif } from '../../interfaces/gif.interface';
 
 @Component({
   selector: 'app-search-page',
-  imports: [],
+  imports: [GifList],
   templateUrl: './search-page.html',
 })
-export default class SearchPage { }
+export default class SearchPage {
+  gifService = inject(GifService);
+
+  gifsResults = signal<Gif[]>([]);
+
+  onSearch(query: string) {
+   this.gifService.searchGifs(query)
+    .subscribe(
+      (resp) => {
+       this.gifsResults.set(resp);
+      }
+    )
+  }
+}
--- src/app/gifs/services/Gifs.Service.ts
+++ src/app/gifs/services/Gifs.Service.ts
@@ -4,6 +4,7 @@ import { environment } from '@environments/environment';
 import type { GiphyResponse } from '../interfaces/giphy.interfaces';
 import type { Gif } from '../interfaces/gif.interface';
 import { GifMapper } from '../mappers/gif.mapper';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +14,6 @@ export class GifService {
   private http = inject(HttpClient);
   trendingGifs = signal<Gif[]>([]);
   trendingGifsLoading = signal(true);
-
-
 
   constructor() {
     this.loadTrendingGifs();
@@ -31,7 +30,28 @@ export class GifService {
       const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
       this.trendingGifs.set(gifs);
       this.trendingGifsLoading.set(false);
-      console.log(gifs);
     })
   }
+
+  searchGifs(query: string) {
+    return this.http.get<GiphyResponse>( `${ environment.giphyUrl }/gifs/search`, {
+      params: {
+        api_key: environment.giphyApiKey,
+        limit: 20,
+        offset: 0,
+        q: query,
+      },
+    })
+    .pipe(
+      map( ( {data} ) => data),
+      map( (items) => GifMapper.mapGiphyItemsToGifArray(items) )
+
+      //TODO: Historial
+    );
+
+  }
+
+
+
+
 }
